52a5aaff4715c3027222161d66b85e42be90789c
milagrosmorenoh <milagrosmhueyo@gmail.com>
2026-01-28 00:39:56 -0300

    Update programacion.py

--- programacion.py
+++ programacion.py
@@ -1,16 +1,41 @@
-# programacion-1
-'''
-entra
-proceso:
-    -ingresa dni , id bici, id casco
-    -devolucion se vuelven a poner dispoinbles ids
-    -tres opciiiones, alquiler , deolver, cierre del dia
-    -verificar si se encuentrara alquilaods,
-    -registro historico de datos ingresado de alquileres
-    -formato csv
-    -verificiar si fueron alquilados a ntigumanetne
+entrada:
+    - datos por teclado:
+        1- Alquilar:
+            datos por teclado:
+                - DNI
+                - numero que identifica bicicleta
+                - numero que identifica casco
+                
+        2- Devolver:
+            datos por teclado:
+                - nro bicicleta
+                - nro casco
+                
+        3- Cierre del día
 
+proceso:
+# dependiendo de la opcion que se elija:
+    1- Alquilar:
+        - pedir datos por teclado
+        - controlar que los elementos no se encuentren ya alquilados
+        - si es asi, se agrega el alquiler al archivo csv con todos los
+        datos ingresados (dni;nro_bici;nro_casco)
+        
+    2- Devolver:
+        - pedir datos por teclado
+        - validar que ambos elementos hayan sido alquilados previamente
+        - liberar bicicleta y casco ingresados
+        
+    3- Cierre del día:
+        - en el archivo de alquileres, contar cuantos alquileres tuvo cada
+        elemento
+        - ordenar este listado de mayor a menor por cant alquileres
+        - buscar articulos sin devolver, identificando dni de quien los tiene
+        en su poder
+        
 salida:
-  -cantidad de alquileres que tuvo cada eleemnto
-  - listado de aritucols aun sin devolver, con el dni de quien lo tiene
-'''
+    - impresion de ambos listados hechos en opcion 3
+    - archivo con todos los alquileres
+    
+        
+    
