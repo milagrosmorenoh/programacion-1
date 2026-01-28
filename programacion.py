3a1468a85a6ae5bb93113e211df6dd7a9b525998
Torressantiago <149216374+TorresSo@users.noreply.github.com>
2026-01-28 00:29:48 -0300

    Implement bike rental process in programacion.py
    Added a new process for bike rental management including input and output details.

--- programacion.py
+++ programacion.py
@@ -1,4 +1,16 @@
 # programacion-1
+'''
+entra
+proceso:
+    -ingresa dni , id bici, id casco
+    -devolucion se vuelven a poner dispoinbles ids
+    -tres opciiiones, alquiler , deolver, cierre del dia
+    -verificar si se encuentrara alquilaods,
+    -registro historico de datos ingresado de alquileres
+    -formato csv
+    -verificiar si fueron alquilados a ntigumanetne
 
-holaaaaaaaaaaaa
-
+salida:
+  -cantidad de alquileres que tuvo cada eleemnto
+  - listado de aritucols aun sin devolver, con el dni de quien lo tiene
+'''
